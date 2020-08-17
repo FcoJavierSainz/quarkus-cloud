c382620cb9f1687da1bae03e230302bb58b7c774
jsainz <fjsainz@mx1.ibm.com>
2020-08-17 14:24:53 -0500

    App setup

--- src/test/java/org/talent/week/NativeCacheResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.talent.week;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCacheResourceIT extends CacheResourceTest {
-
-    // Execute the same tests but in native mode.
-}
